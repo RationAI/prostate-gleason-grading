d3fdcd6b9a4490622d69781cbc4df3f8b377842f
Marcel Krkoš <marcelkrkos@gmail.com>
2026-07-10 12:51:36 +0000

    exp: virchow2 linear probe training scripts and configs

--- scripts/training/run_train_virchow2_adamw.py
+++ scripts/training/run_train_virchow2_adamw.py
@@ -2,16 +2,17 @@ from kube_jobs import submit_job
 
 
 submit_job(
-    job_name="prostate-gleason-attach-embeddings",
+    job_name="prostate-gleason-train-virchow2-lbfgs",
     username=...,
     image="cerit.io/rationai/base:2.0.6",
-    cpu=10,
-    memory="120Gi",
+    cpu=8,
+    gpu=...,
+    memory="64Gi",
     public=False,
     script=[
         "git clone https://github.com/RationAI/prostate-gleason-grading.git workdir",
         "cd workdir",
         "uv sync",
-        "uv run -m preprocessing.attach_embeddings +experiment=preprocessing/mmci2k_224_attach_virchow2_embeddings",
+        "uv run -m ml +experiment=/training/mmci2k/embeddings/virchow2/adamw datamodule.fold=... datamodule.invert_fold_selection=... model.lr=...",
     ],
 )
---
+++ scripts/training/run_train_virchow2_lbfgs.py
@@ -0,0 +1,18 @@
+from kube_jobs import submit_job
+
+
+submit_job(
+    job_name="prostate-gleason-train-virchow2-lbfgs",
+    username=...,
+    image="cerit.io/rationai/base:2.0.6",
+    cpu=8,
+    gpu=...,
+    memory="64Gi",
+    public=False,
+    script=[
+        "git clone https://github.com/RationAI/prostate-gleason-grading.git workdir",
+        "cd workdir",
+        "uv sync",
+        "uv run -m ml +experiment=/training/mmci2k/embeddings/virchow2/lbfgs datamodule.fold=... model.weight_decay=...",
+    ],
+)
