ec622a53cef6232bf2ad0f924c4b09eff320cd7b
MarcelKrkos <marcelkrkos@gmail.com>
2026-08-30 20:00:41 +0000

    feat: heatmaps script

---
+++ scripts/training_and_evaluation/virchow2/heatmaps.py
@@ -0,0 +1,24 @@
+from kube_jobs import submit_job
+
+
+submit_job(
+    job_name="prostate-gleason-heatmaps",
+    username=...,
+    image="cerit.io/rationai/base:2.0.6",
+    cpu=8,
+    gpu=...,
+    memory="30Gi",
+    public=False,
+    script=[
+        "export MLFLOW_TRACKING_URI=http://mlflow-s3.rationai-mlflow",
+        "git clone https://github.com/RationAI/prostate-gleason-grading.git workdir",
+        "cd workdir",
+        "uv sync",
+        """uv run -m ml \
+           experiment=/training_and_evaluation/virchow2/heatmaps \
+           experiment/training_and_evaluation/virchow2/data=mmci2k \
+           experiment/training_and_evaluation/virchow2/model=lbfgs \
+           model.weight_decay=0 checkpoint=...\
+        """,
+    ],
+)
