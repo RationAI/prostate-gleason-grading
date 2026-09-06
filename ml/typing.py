bd14c1bf02d59225946ac36fbe3458700871e479
MarcelKrkos <marcelkrkos@gmail.com>
2026-09-06 14:59:51 +0000

    feat: bag of samples types

--- ml/typing.py
+++ ml/typing.py
@@ -15,8 +15,18 @@ class MetadataBatch(TypedDict):
     y: Tensor
 
 
+class BagMetadata(TypedDict):
+    slide: str
+    x: Tensor
+    y: Tensor
+
+
 type LabeledSample = tuple[Tensor, Metadata, Tensor]
 type UnlabeledSample = tuple[Tensor, Metadata]
 
 type LabeledSampleBatch = tuple[Tensor, MetadataBatch, Tensor]
 type UnlabeledSampleBatch = tuple[Tensor, MetadataBatch]
+
+type FullyLabeledBag = tuple[Tensor, BagMetadata, Tensor, Tensor]
+type WeaklyLabeledBag = tuple[Tensor, BagMetadata, Tensor]
+type UnlabeledBag = tuple[Tensor, BagMetadata]
